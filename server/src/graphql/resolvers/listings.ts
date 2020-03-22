283d3a35381784b529c732e9bcfe6ac866dd1d4f
Andrew Watkins <andrew.watkinz@gmail.com>
2020-03-22 09:56:56 -0700

    refactor: modularize resolvers

---
+++ server/src/graphql/resolvers/listings.ts
@@ -0,0 +1,35 @@
+import {IResolvers} from 'apollo-server-express'
+import {Database, Listing} from '../../lib/types'
+import {ObjectId} from 'mongodb'
+
+export const listingResolvers: IResolvers = {
+    Query: {
+        listings: async (
+            _root,
+            _args,
+            {db}: {db: Database}
+        ): Promise<Listing[]> => {
+            return await db.listings.find().toArray()
+        },
+    },
+    Mutation: {
+        deleteListing: async (
+            _root,
+            {id}: {id: string},
+            {db}: {db: Database}
+        ): Promise<Listing> => {
+            const result = await db.listings.findOneAndDelete({
+                _id: new ObjectId(id),
+            })
+
+            if (!result.value) {
+                throw new Error('could not delete listing')
+            }
+
+            return result.value
+        },
+    },
+    Listing: {
+        id: (listing: Listing): string => listing._id.toString(),
+    },
+}
